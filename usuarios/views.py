d5b7dfc817368acc72c80af68d609407931ca889
Gyl Monteiro <gyl.tecno.especialista@gmail.com>
2024-01-16 12:30:56 -0300

    feat: created mensages to display in cases errors

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.contrib.messages import constants
+from django.contrib import messages
 
 # Create your views here.
 def cadastro (request):
     if request.method == "GET":
         return render (request, 'cadastro.html')
+    else:
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+
+        if not senha == confirmar_senha:
+            messages.add_message(request, constants.ERROR, 'As senhas não são idênticas')
+            return redirect('/usuarios/cadastro')
+
+        user = User.objects.filter(username__iexact=username)
+
+        if user.exists():
+            messages.add_message(request, constants.ERROR, 'O usuário já é existente')
+            return redirect('/usuarios/cadastro')
+        
+        try:
+            user = User.objects.create_user(username=username, password=senha)
+            return redirect('/usuarios/login')
+        except:
+            messages.add_message(request, constants.ERROR, 'Erro interno do seriviço')
+            return redirect('/usuarios/cadastro')
